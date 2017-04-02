69c8966c40461b8bad4e32dae3109b5a9e09bc71
Timo <timo.stoehr@googlemail.com>
2017-04-02 16:27:03 +0200

    update

--- app/src/main/java/com/example/android/battletechquiz/MainActivity.java
+++ app/src/main/java/com/example/android/battletechquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.battletechquiz;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -9,7 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.android.battletechquiz.R.id.question;
 
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
     private Button answerC;
     private Button answerD;
     private int questionCursor;
+    private int correctAnswers = 0;
+    private String correctAnswer;
+    private int questionCount = 4;
+    private View questionSection;
+    private EditText playerNameView;
+    private Button startButton;
+    private String playerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        questionSection = findViewById(R.id.questionSection);
+        playerNameView = (EditText)findViewById(R.id.playerName);
+        startButton = (Button)findViewById(R.id.start);
     }
 
     @Override
@@ -38,11 +51,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startQuiz(View view){
-        /* hide start button */
-        view.setVisibility(View.INVISIBLE);
+        /* hide start Button and EditText */
+        startButton.setVisibility(View.INVISIBLE);
+        playerNameView.setVisibility(View.INVISIBLE);
+        /* get player name for final scores */
+        playerName = playerNameView.getText().toString();
 
         /* Initialize all questionSection Views*/
-        View questionSection = findViewById(R.id.questionSection);
         Question = (TextView) findViewById(question);
         answerA = (Button) findViewById(R.id.answerA);
         answerB = (Button) findViewById(R.id.answerB);
@@ -73,26 +88,141 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void giveAnswer(View view){
-        if (questionCursor < 3) {
+    public void highlightAnswer(String answer){
+        switch(correctAnswer) {
+            case "answerA":
+                answerA.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.answerCorrect));
+                break;
+            case "answerB":
+                answerB.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.answerCorrect));
+                break;
+            case "answerC":
+                answerC.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.answerCorrect));
+                break;
+            case "answerD":
+                answerD.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.answerCorrect));
+                break;
+        }
+
+        sleep();
+
+    }
+
+    public void giveAnswerA(View view){
+        Button answer = (Button)view;
+
+        Log.v("Answer Button text", answer.getText().toString());
+
+        /*highlightAnswer(correctAnswer);*/
+
+        if (correctAnswer.equals("answerA")) {
+            correctAnswers = correctAnswers + 1;
+            Log.v("correctAnswers", String.valueOf(correctAnswers));
+        }
+
+        if (questionCursor <= questionCount) {
             setQuestion(questionCursor);
         }
         else {
-            /* show finish card */
+            Log.v("correctAnswers final", String.valueOf(correctAnswers));
+            questionSection.setVisibility(View.INVISIBLE);
+            showScore();
         }
 
+    }
 
+    public void giveAnswerB(View view){
+        Button answer = (Button)view;
+
+        Log.v("Answer Button text", answer.getText().toString());
+
+        /*highlightAnswer(correctAnswer);*/
+
+        if (correctAnswer.equals("answerB")) {
+            correctAnswers = correctAnswers + 1;
+            Log.v("correctAnswers", String.valueOf(correctAnswers));
+        }
+
+        if (questionCursor <= questionCount) {
+            setQuestion(questionCursor);
+        }
+        else {
+            Log.v("correctAnswers final", String.valueOf(correctAnswers));
+            questionSection.setVisibility(View.INVISIBLE);
+            showScore();
+        }
+
+    }
+
+    public void giveAnswerC(View view){
+        Button answer = (Button)view;
+
+        Log.v("Answer Button text", answer.getText().toString());
+
+        /*highlightAnswer(correctAnswer);*/
+
+        if (correctAnswer.equals("answerC")) {
+            correctAnswers = correctAnswers + 1;
+            Log.v("correctAnswers", String.valueOf(correctAnswers));
+        }
+
+        if (questionCursor <= questionCount) {
+            setQuestion(questionCursor);
+        }
+        else {
+            Log.v("correctAnswers final", String.valueOf(correctAnswers));
+            questionSection.setVisibility(View.INVISIBLE);
+            showScore();
+        }
+
+    }
+
+    public void giveAnswerD(View view){
+        Button answer = (Button)view;
+
+        Log.v("Answer Button text", answer.getText().toString());
+
+        /*highlightAnswer(correctAnswer);*/
+
+        if (correctAnswer.equals("answerD")) {
+            correctAnswers = correctAnswers + 1;
+            Log.v("correctAnswers", String.valueOf(correctAnswers));
+        }
+
+        if (questionCursor <= questionCount) {
+            setQuestion(questionCursor);
+        }
+        else {
+            Log.v("correctAnswers final", String.valueOf(correctAnswers));
+            questionSection.setVisibility(View.INVISIBLE);
+            showScore();
+        }
+
+    }
+
+    public void showScore(){
+        if (correctAnswers == questionCount){
+            Toast.makeText(getApplicationContext(), "Awesome " + playerName + "!\n" + "You scored " + String.valueOf(correctAnswers) + " correct Answers out of " + questionCount + ".", Toast.LENGTH_LONG).show();
+        }
+        else if (correctAnswers > 0){
+            Toast.makeText(getApplicationContext(), "Congratulations " + playerName + "!\n" + "You scored " + String.valueOf(correctAnswers) + " correct Answers out of " + questionCount + ".", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Sorry " + playerName + ".\n" + "You scored " + String.valueOf(correctAnswers) + " correct Answers out of " + questionCount + ".", Toast.LENGTH_LONG).show();
+        }
+
+        startButton.setVisibility(View.VISIBLE);
+        playerNameView.setText("");
+        playerNameView.setVisibility(View.VISIBLE);
     }
 
     public void setQuestion(int cursor){
         Resources r = getResources();
 
-        String cad="Question";
+        String identifier="Question";
 
-        cad = cad+Integer.toString(cursor);
-        int currentQuestion = getResources().getIdentifier(cad, "array", getPackageName());
-        Log.v("currentQuestion", Integer.toString(currentQuestion));
-        Log.v("questionCursor after", Integer.toString(questionCursor));
+        identifier = identifier+Integer.toString(cursor);
+        int currentQuestion = getResources().getIdentifier(identifier, "array", getPackageName());
 
         String[]question = r.getStringArray(currentQuestion);
 
@@ -101,8 +231,17 @@ public class MainActivity extends AppCompatActivity {
         answerB.setText(question[2]);
         answerC.setText(question[3]);
         answerD.setText(question[4]);
+        correctAnswer = question[5];
 
         questionCursor = cursor + 1;
+    }
+
+    public void sleep(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
